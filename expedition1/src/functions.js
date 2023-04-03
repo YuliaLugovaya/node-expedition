e3ecaeea98be27628f947786e1f81f5e1cb2f72b
YuliaLugovaya <yulialugovayaphoto@gmail.com>
2023-04-03 22:16:38 +0300

    feat: add solutions for part one

--- expedition1/src/functions.js
+++ expedition1/src/functions.js
@@ -1,35 +1,130 @@
+const fs = require('fs');
 
-
-//позволяет выбрать самого опытного капитана
+// позволяет выбрать самого опытного капитана
 function getRightCaptain() {
+  const PATH = './src/crew.txt';
+  const readFile = fs.readFileSync(PATH, 'utf-8');
+  const arrayCrew = readFile.split('\r\n');
 
+  let arrayCrewMore = [];
+  for (let i = 1; i < arrayCrew.length; i++) {
+    let el = arrayCrew[i];
+    arrayCrewMore.push(el.split(', '));
+  }
+
+  let arrayOnlyCaptains = arrayCrewMore.filter((el) => {
+    return el.includes('Капитан');
+  })
+
+  let bestCaptain = arrayOnlyCaptains.sort((a, b) => {
+    a = a[3];
+    b = b[3];
+    return +b - +a
+  })
+  return bestCaptain[0].join(', ');
 }
+console.log(getRightCaptain());
 
-//позволяет выбрать самого опытного врача среди женщин
+// позволяет выбрать самого опытного врача среди женщин
 function getRightDoc() {
+  const PATH = './src/crew.txt';
+  const readFile = fs.readFileSync(PATH, 'utf-8');
+  const arrayCrew = readFile.split('\r\n');
 
+  let arrayCrewMore = [];
+  for (let i = 1; i < arrayCrew.length; i++) {
+    let el = arrayCrew[i];
+    arrayCrewMore.push(el.split(', '));
+  }
+
+  let arrayOnlyDoctorsWomen = arrayCrewMore.filter((el) => {
+    return el.includes('Врач') && el.includes('ж');
+  })
+
+  let bestDoctor = arrayOnlyDoctorsWomen.sort((a, b) => {
+    a = a[3];
+    b = b[3];
+    return +b - +a
+  })
+  return bestDoctor[0].join(', ');
 }
 
-//позволяет выбрать всех бортмехаников
+// позволяет выбрать всех бортмехаников
 function getAllEngineer() {
+  const PATH = './src/crew.txt';
+  const readFile = fs.readFileSync(PATH, 'utf-8');
+  const arrayCrew = readFile.split('\r\n');
 
+  let arrayCrewMore = [];
+  for (let i = 1; i < arrayCrew.length; i++) {
+    let el = arrayCrew[i];
+    arrayCrewMore.push(el.split(', '));
+  }
+
+  let arrayOnlyEngineers = arrayCrewMore.filter((el) => {
+    return el.includes('Бортмеханик');
+  }).map((el) => el.join(', '));
+
+  return arrayOnlyEngineers;
 }
 
-//Позволяет отобрать все марсоходы
-function getAllRover(){
+// Позволяет отобрать все марсоходы
+function getAllRover() {
+  const PATH = './src/equipment.txt';
+  const readFile = fs.readFileSync(PATH, 'utf-8');
+  const arrayEquipment = readFile.split('\r\n');
 
+  let arrayEquipmentMore = [];
+  for (let i = 1; i < arrayEquipment.length; i++) {
+    let el = arrayEquipment[i];
+    arrayEquipmentMore.push(el.split(', '));
+  }
+
+  let arrayOnlyRovers = arrayEquipmentMore.filter((el) => {
+    return el.includes('марсоход');
+  }).map((el) => el.join(', '));
+
+  return arrayOnlyRovers;
 }
 
-//позволяет выбрать только те марсоходы, которые смогут прослужить больше 3 лет
-function getRightRovers(){
+// позволяет выбрать только те марсоходы, которые смогут прослужить больше 3 лет
+function getRightRovers() {
+  const PATH = './src/equipment.txt';
+  const readFile = fs.readFileSync(PATH, 'utf-8');
+  const arrayEquipment = readFile.split('\r\n');
 
+  let arrayEquipmentMore = [];
+  for (let i = 1; i < arrayEquipment.length; i++) {
+    let el = arrayEquipment[i];
+    arrayEquipmentMore.push(el.split(', '));
+  }
+
+  let arrayOnlyRovers = arrayEquipmentMore.filter((el) => {
+    return el.includes('марсоход') && el[2] > 3;
+  }).map((el) => el.join(', '));
+
+  return arrayOnlyRovers;
 }
 
-//позволяет выбрать ракету с максимальной дальностью полёта
-function getRightRocket(){
+// позволяет выбрать ракету с максимальной дальностью полёта
+function getRightRocket() {
+  const PATH = './src/rockets.txt';
+  const readFile = fs.readFileSync(PATH, 'utf-8');
+  const arrayRockets = readFile.split('\r\n');
 
+  let arrayRocketsMore = [];
+  for (let i = 1; i < arrayRockets.length; i++) {
+    let el = arrayRockets[i];
+    arrayRocketsMore.push(el.split(', '));
+  }
+
+  let bestRocket = arrayRocketsMore.sort((a, b) => {
+    a = a[2];
+    b = b[2];
+    return +b - +a
+  })
+  return bestRocket[0].join(', ');
 }
-
 
 module.exports = {
   getRightCaptain,
@@ -37,5 +132,5 @@ module.exports = {
   getRightDoc,
   getAllRover,
   getRightRovers,
-  getRightRocket
+  getRightRocket,
 };
